e30c0e863b1fd3539c4ed40964295e2bef6bcf3a
kad108 <kadamba@index.hu>
2017-04-09 20:58:22 +0100

    4.3 controller, views, route ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :products
+  
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
-  root 'static_pages#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#landing_page'
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
