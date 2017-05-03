8ca33cffda5f621e9dc47fe35eaac9327b2fb695
kad108 <kadamba@index.hu>
2017-05-03 12:09:38 +0100

    fixed static_pages_controller.rb & landing_landing.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,11 @@
 class StaticPagesController < ApplicationController
+
   def index
   end
-end
 
-class StaticPagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
     @products = Product.limit(3)
   end
+
 end
