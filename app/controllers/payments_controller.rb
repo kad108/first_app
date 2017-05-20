dc6040b1d636e60727c8945b752579f65eff34bc
kad108 <kadamba@index.hu>
2017-05-20 18:08:42 +0100

    6.6 Payments Controller extended

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,27 @@
+class PaymentsController < ApplicationController
+
+	def create
+		token = params[:stripeToken]
+		@product = Product.find(params[:product_id])
+		@user = current_user
+
+		# Create the charge on Stripe's servers to charge the user's card
+		begin
+			charge = Stripe::Charge.create(
+				amount: (@product.price*100),
+				currency: "usd",
+				source: token,
+				decription: params[:stripeEmail]
+			)
+
+		if charge.paid
+			Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price)
+		end
+
+		rescue Stripe::CardError => e
+			# The card has benn declined
+		end
+	redirect_to product_path(@product), notice: 'Thank you for your order!'
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   resources :users
-  resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-
   resources :products do
     resources :comments
   end
@@ -14,6 +13,7 @@ Rails.application.routes.draw do
 	get 'static_pages/landing_page'
   root 'products#index'
   post 'static_pages/thank_you'
+  post 'payments/create'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
