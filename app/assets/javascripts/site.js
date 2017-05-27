a9e89e8a72af0d6dae8c46b7d574f39b1bd72c02
kad108 <kadamba@index.hu>
2017-05-27 15:30:11 +0100

    ElevateZoom ready

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,13 @@
 $(document).on('turbolinks:load', function(){
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
+    $('.rating').raty({ path: "/assets", scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+      score: function() {
+       return $(this).attr('data-score');
+      }
+   });
+
+   $('.img-zoom').elevateZoom({
+   	zoomType: "lens", lensShape: "round", lensSize: 200
+   });
 });
