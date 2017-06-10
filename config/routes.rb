1f40b2edaa7ebf9a180494ee62bf5cc9bf6dd6f2
kad108 <kadamba@index.hu>
2017-06-10 19:22:22 +0100

    6.13 Real-Time Updates with ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  
+  mount ActionCable.server => '/cable'
   devise_for :users
   resources :users
   resources :products do
@@ -14,6 +15,5 @@ Rails.application.routes.draw do
   root 'products#index'
   post 'static_pages/thank_you'
   post 'payments/create'
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
