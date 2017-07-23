df9816e7292b122313c0fb520bc7614b103611e2
kad108 <kadamba@index.hu>
2017-07-23 14:27:33 +0100

    fixed products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,9 +64,8 @@ class ProductsController < ApplicationController
 
   # DELETE /products/1
   # DELETE /products/1.json
-
-  @product = Product.find(params[:id])
   def destroy
+    @product = Product.find(params[:id])
     @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
