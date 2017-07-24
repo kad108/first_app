1c5d3ad13fa8190a2a55c4c253da8b3fecc57c68
kad108 <kadamba@index.hu>
2017-07-24 18:10:50 +0100

    6.14 ActiveJob &  Advanced ActionCable ready

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -8,9 +8,7 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   },
 
   received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
-    $(".alert.alert-info").show();
-    // console.log(data);
+    $('.alert.alert-info').show();
     $('.product-reviews').prepend(data.comment);
     $("#average-rating").attr('data-score', data.average_rating);
     refreshRating();
@@ -20,7 +18,6 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     return this.perform('listen', {
       product_id: $("[data-product-id]").data("product-id")
     });
-
   }
 });
 
